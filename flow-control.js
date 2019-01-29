7f10cfdcc1c69497a03bfd937e109d2a8da0aa09
Teamese Felix <teamesef@gmail.com>
2019-01-29 04:28:33 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,9 +6,9 @@ function basicTeenager(age) {
 
 function teenager(age) {
   if (13-19) {
-    //You are a teenager!() if `age` is truthy
+    //You are a teenager!() if `13-19` is truthy
   } else {
-    //You are not a teenager() if `age` is falsey
+    //You are not a teenager() if `13-19` is falsey
   }
 }
 

202bc59fab3ced1ccdc5eb857e3b13f20d2bef47
Teamese Felix <teamesef@gmail.com>
2019-01-29 06:00:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,6 +27,7 @@ return (age >= 13 && age <=19) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
+switch(age)
   case 13:
     return "You are a teenager"
   case 14:

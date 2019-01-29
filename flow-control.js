0ac55fe928e3d8919e304122593f7357f8858da9
Teamese Felix <teamesef@gmail.com>
2019-01-29 04:12:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,15 @@
 function basicTeenager(age) {
-
+  if (13-19) {
+    //You are a teenager!()
+  }
 }
 
 function teenager(age) {
-
+  if (13-19) {
+    //You are a teenager!()
+  } else {
+    //You are not a teenager()
+  }
 }
 
 function ageChecker(age) {
